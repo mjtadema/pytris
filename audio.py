89575179dc49861a5d5af95af4ee3a5890d4db6b
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-05-24 00:44:02 +0200

    Lorenzo's song improvements

--- audio.py
+++ audio.py
@@ -4,18 +4,23 @@
 import numpy as np
 import simpleaudio as sa
 
-song = ["E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","Ah","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A"]
-timing = [2] + [1,1,2] * 4 + [2,2,2] * 2 + [2,1,2] + [1,1,2] + [1,2] + [1,1,2] * 2 + [2,2,2,2]
+# Song improved by Lorenzo Gaifas
+song = ["E","B","C"] + ["D","C","B"] + ["A","A","C"] + ["E","D","C"] + ["B","B","C"] + ["D","E","C","A","A"] + \
+       ["P","P"] + ["D","F"] + \
+       ["Ah","G","F"] + ["E","E","C"] + ["E","D","C"] + ["B","B","C"] + ["D","E","C","A","A"] + \
+       ["P"]
+timing = [2,1,1] * 5 + [2,2,2,2,2] + [2,1] + [2,1] + [2,1,1] * 4 + [2,2,2,2,2] + [2]
 
 # calculate note frequencies
 A_freq = 440
 
-freqs = [
+freqs = [0] + [
     A_freq * 2 ** (i / 12)
     for i in range(13)
     ]
 
-notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#','Ah']
+notes = ['P','A','A#','B','C','C#','D','D#','E','F','F#','G','G#','Ah']
+
 note_to_freq = {
         n: f
         for n, f in zip(notes, freqs)
@@ -24,19 +29,11 @@ note_to_freq = {
 # get timesteps for each sample, T is note duration in seconds
 sample_rate = 44100
 T = 0.25
-#t = np.linspace(0, T, T * sample_rate, False)
 t = lambda factor: np.linspace(0, T*factor, T*factor * sample_rate, False)
-
-# generate sine wave notes
-#A_note = np.sin(A_freq * t * 2 * np.pi)
-#Csh_note = np.sin(Csh_freq * t * 2 * np.pi)
-#E_note = np.sin(E_freq * t * 2 * np.pi)
 
 def note_to_sine(note, time):
     f = note_to_freq[note]
     return np.sin(f * t(time) * 2 * np.pi) 
-
-
 
 song_sines = [
         note_to_sine(note, time)
