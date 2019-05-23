37600b0e88c128a9e0fc3a60022ad4797d5b2606
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-05-23 21:59:45 +0200

    Added sound!

---
+++ audio.py
@@ -0,0 +1,59 @@
+# Code based on https://simpleaudio.readthedocs.io/en/latest/tutorial.html#waveobject-s
+# Song composition based on http://www.piano-keyboard-guide.com/how-to-play-the-tetris-theme-song-easy-piano-tutorial-korobeiniki/
+
+import numpy as np
+import simpleaudio as sa
+
+song = ["E","B","C","D","C","B","A","A","C","E","D","C","B","C","D","E","C","A","A","D","F","Ah","G","F","E","C","E","D","C","B","B","C","D","E","C","A","A"]
+timing = [2] + [1,1,2] * 4 + [2,2,2] * 2 + [2,1,2] + [1,1,2] + [1,2] + [1,1,2] * 2 + [2,2,2,2]
+
+# calculate note frequencies
+A_freq = 440
+
+freqs = [
+    A_freq * 2 ** (i / 12)
+    for i in range(13)
+    ]
+
+notes = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#','Ah']
+note_to_freq = {
+        n: f
+        for n, f in zip(notes, freqs)
+        }
+
+# get timesteps for each sample, T is note duration in seconds
+sample_rate = 44100
+T = 0.25
+#t = np.linspace(0, T, T * sample_rate, False)
+t = lambda factor: np.linspace(0, T*factor, T*factor * sample_rate, False)
+
+# generate sine wave notes
+#A_note = np.sin(A_freq * t * 2 * np.pi)
+#Csh_note = np.sin(Csh_freq * t * 2 * np.pi)
+#E_note = np.sin(E_freq * t * 2 * np.pi)
+
+def note_to_sine(note, time):
+    f = note_to_freq[note]
+    return np.sin(f * t(time) * 2 * np.pi) 
+
+
+
+song_sines = [
+        note_to_sine(note, time)
+        for note, time in zip(song, timing)
+        ]
+
+# concatenate notes
+audio = np.hstack(song_sines)
+# normalize to 16-bit range
+audio *= 32767 / np.max(np.abs(audio))
+# dampen a bit
+audio *= 0.255555
+# convert to 16-bit data
+audio = audio.astype(np.int16)
+
+# start playback
+def start():
+    while True:
+        play_obj = sa.play_buffer(audio, 1, 2, sample_rate)
+        play_obj.wait_done()
