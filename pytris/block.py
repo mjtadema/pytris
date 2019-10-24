bbf677ace484c2cbbc9d7d4addc41f82495f351e
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-10-24 15:03:48 +0200

    Added curses colors, but not yet in blocks Added mobile attribute Added move decorator Added collision method

--- pytris/block.py
+++ pytris/block.py
@@ -21,12 +21,21 @@
 #SOFTWARE.
 
 import numpy as np
+import curses
+
+curses.use_default_colors()
+for i in range(0, curses.COLORS):
+    curses.init_pair(i, -1, i);
+"""
+
+"""
 
 class Block():
     """
     Base block class, is inherited from to form individual blocks
+    Collisions are checked upon movement
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, game, *args, **kwargs):
         """
         Base block object
             init()                  : Spawns new block
@@ -49,8 +58,10 @@ class Block():
         """
 
         self.grid = kwargs['grid']
+        self.game = game
         grid_y, grid_x = self.grid.gridsize
         insert_point = (0, grid_x // 2)
+        self.mobile = True
 
         # Initialize block definition
         self.anchor = [insert_point]
@@ -65,33 +76,97 @@ class Block():
         """
         return np.add(self.anchor[-1], self.states[self.rotation[-1]])
 
+    def to_grid(self):
+        """
+        Moves the block to grid when it has reached the bottom
+        :return: None
+        """
+        for x, y in self.position():
+            self.grid[x][y] = self.color
+
+    def move(self, move_func):
+        """
+        Decorator for movement functions
+        Movement methods return the appropriate move log to
+        revert the move
+        Checks for collision, first check if bottom is hit.
+        If block collided with the bottom of the grid or the
+        top of a block, move the block to grid
+        If block collided with a wall (sideways),
+        revert the move by popping the last item
+        Finally automatically draw the block to the screen
+        :param move_func: A function that moves the block
+        :return: Boolean signifying collision
+        """
+        def wrapper():
+            if not self.mobile:
+                raise Exception("Block is not mobile but trying to move block")
+            move_log = move_func()
+            # First check if a collision has occurred
+            if self.collision():
+                # If it has, revert the move
+                move_log.pop(-1)
+                # Check if the last move was downward
+                if move_func == self.down:
+                    # If it was, it's a bottom collision
+                    self.to_grid()
+                    self.mobile = False
+                    return
+            # Finally abstract away screen drawing
+            self.game.screen.block()
+        return wrapper
+
+    def collision(self):
+        """
+        Check if any block position intersects with a block on
+        the grid.
+        :return: Boolean
+        """
+        for x, y in self.position():
+            if x == self.grid.grid_x:
+                return True
+            if self.grid[x][y] != 0:
+                return True
+        return False
+
+    @move
     def down(self):
         now = self.anchor[-1]
         next = tuple(np.add(now, (0, 1)))
         self.anchor.append(next)
+        return self.anchor
 
+    @move
     def up(self):
         now = self.anchor[-1]
         next = tuple(np.subtract(now, (0, 1)))
         self.anchor.append(next)
+        return self.anchor
 
+    @move
     def left(self):
         now = self.anchor[-1]
         next = tuple(np.subtract(now, (1, 0)))
         self.anchor.append(next)
+        return self.anchor
 
+    @move
     def right(self):
         now = self.anchor[-1]
         next = tuple(np.add(now, (1, 0)))
         self.anchor.append(next)
+        return self.anchor
 
+    @move
     def clockwise(self):
         tmp = 0
         if len(self.states) > 1:
             tmp = self.rotation[-1] + 1
             tmp %= len(self.states) - 1
         self.rotation.append(tmp)
+        return self.anchor
 
+    @move
     def countercw(self):
         tmp = 0
         if len(self.states) > 1:
@@ -99,6 +174,10 @@ class Block():
             if tmp < 0:
                 tmp = len(self.states) - 1
         self.rotation.append(tmp)
+        return self.anchor
+
+
+
 
 class I(Block):
     def __init__(self, *args, **kwargs):
