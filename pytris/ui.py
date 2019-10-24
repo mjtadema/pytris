b5de0202da56a0979db5e6a452bd0e0e617c86b4
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-10-24 16:12:21 +0200

    Added a decorator to skip curses calls when debugging

--- pytris/ui.py
+++ pytris/ui.py
@@ -15,21 +15,56 @@ class Screen():
     It displays output but also records input
     """
 
-    def __init__(self, game, screen, *args, **kwargs):
+    def __init__(self, game, *args, screen = None, **kwargs):
         self.game = game
         self.grid_y, self.grid_x = self.game.gridsize
         self.screen = screen
         self.border()
-        curses.curs_set(0)
-        self.screen.nodelay(True)
+        if not self.game.debug:
+            curses.use_default_colors()
+            for i in range(0, curses.COLORS):
+                curses.init_pair(i, -1, i);
+            curses.curs_set(0)
+            self.screen.nodelay(True)
 
+    def curses(curses_func):
+        """
+        Decorator for functions dealing with a curses screen
+        If in debugging mode, curses is not initialized so these functions would fail
+        This decorator simply passes those functions if in debug mode
+        :return: None
+        """
+        def wrapper(self, *args, **kwargs):
+            if self.game.debug:
+                return
+            return curses_func(*args, **kwargs)
+        return wrapper
 
     def addstr(self, *args, **kwargs):
         self.screen.addstr(*args, **kwargs)
 
+    @curses
+    def command(self):
+        """
+        Attempt to get a command and execute it
+        Available commands:
+        up
+        down
+        left
+        right
+        clockwise
+        counter clockwise
+        pause
+        exit
+        :return: Bool for success status
+        """
+        pass
+
+    @curses
     def refresh(self, *args, **kwargs):
         self.screen.refresh(*args, **kwargs)
 
+    @curses
     def block(self):
         """
         Draw a mobile block to the screen
@@ -38,6 +73,7 @@ class Screen():
         """
         pass
 
+    @curses
     def next(self):
         """
         Draw next block to screen in appropriate area
@@ -45,6 +81,7 @@ class Screen():
         """
         pass
 
+    @curses
     def data(self):
         """
         Draw data to appropriate places
@@ -52,6 +89,7 @@ class Screen():
         """
         pass
 
+    @curses
     def border(self):
         """
         Print the border at initialization
@@ -65,7 +103,8 @@ class Screen():
             self.addstr(Y, 0, vertical)
         self.addstr(border_height+1, 0, horizontal)
         self.screen.refresh()
-    
+
+    @curses
     def print(self, *message):
         """
         print a message in the appropriate place
@@ -75,6 +114,7 @@ class Screen():
         self.screen.addstr(y_print, x_print, " ".join(message))
         self.screen.refresh()
 
+    @curses
     def grid(self, grid):
         """
         Draw a matrix over the current grid
