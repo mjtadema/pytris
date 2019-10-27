cee7a1640ffbc0c621e01fc50c6c57b374297b05
Matthijs Tadema <M.J.Tadema@gmail.com>
2019-10-27 17:50:52 +0100

    Large rewrite of the main game loop, is now simple and works

--- pytris/game.py
+++ pytris/game.py
@@ -30,14 +30,7 @@ from multiprocessing import Process
 from pathlib import Path
 import copy
 import random
-
-report = ''
-score_file = f"{Path.home()}/.pytris_highscore"
-name = 'nobody'
-highscore = 0
-block = "curses."
-block_list = []
-p_audio = ''
+import getpass
 
 class Game():
     """
@@ -51,24 +44,17 @@ class Game():
         queue   : queue of next blocks
         screen  : abstraction to curses
     """
-    def __init__(self, *args, gridsize = (20, 10), debug = False, screen = None, **kwargs):
+    def __init__(self, debug = False, screen = None):
         """ 
         Initialize game state
 
         """
         # Move arguments to attributes
-        self.gridsize = gridsize
         self.debug = debug
-
-        # Start audio
-        try:
-            if kwargs['audio']:
-                self.p_audio = start_audio()
-        except KeyError:
-            pass
 
         # Initialize screen
         self.screen = Screen(self, screen)
+        self.screen.print("Debugging: " + str(self.debug))
 
         # Initialize grid
         self.grid = Grid(self)
@@ -76,7 +62,7 @@ class Game():
         # Initialize block queue
         self.queue = Queue(self)
 
-        # Initialize first block
+        # Pop the first block
         self.block = self.queue.pop()
 
         # Initialize some values
@@ -86,20 +72,46 @@ class Game():
             # When debugging just put maximum speed
             self.speed = 0.0
         else:
-            self.speed = 1.0
+            self.speed = 0.8
+        self.screen.print("Speed: " + str(self.speed))
         self.factor = 0.6
         self.level = 1
         self.t = 0
-        self.username = "test"
-        self.highscore = 1000
+
+        self.read_highscore()
+        self.screen.data()
 
         self.screen.print("Initialized game")
+
+    def read_highscore(self):
+        try:
+            with open(f"{Path.home()}/.pytris_highscore") as f_high:
+                self.username = str(f_high.readline().strip())
+                self.highscore = int(f_high.readline().strip())
+        except:
+            self.username = "Nobody"
+            self.highscore = 0
+
+    def write_highscore(self):
+        if self.score > self.highscore:
+            self.username = getpass.getuser()
+            try:
+                with open(f"{Path.home()}/.pytris_highscore", 'w') as f_high:
+                    f_high.write(str(self.username + "\n"))
+                    f_high.write(str(self.score))
+            except:
+                pass # Else just fuck it
 
     def add_score(self, score_to_add):
         """
         Adds the score to the current score in game
         """
         self.score += score_to_add
+        if self.score // self.level >= 20:
+            self.level += 1
+            self.speed *= self.factor
+            self.screen.print("Speed is now: " + str(self.speed))
+            self.screen.data()
         # Refresh the data on screen
         self.screen.data()
 
@@ -118,87 +130,39 @@ class Game():
         """
         The main game loop
         """
+        while not self.gameover:
 
-        try:
-            while not self.gameover:
+            # Pop a new block from queue
+            self.block = self.queue.pop()
+            self.screen.print("Popped a block")
 
-                if not self.block.mobile:
-                    # Pop a new block from queue
-                    self.block = self.queue.pop()
-                    self.screen.print("Popped a block")
+            while self.block.mobile:
 
-                # Keep track of game ticks
-                # speed basically determines the time a tick takes
-                while self.block.mobile:
-                    # Move the block down every "tick"
-                    if self.tick():
-                        if not self.block.down():
-                            break
-                        self.screen.print("Moved down")
-                        if self.debug:
-                            # If debugging do a random move
-                            self.block.random_move()
-
-                    # Try to get a command every cycle
-                    if not self.debug:
-                        try:
-                            # Get a command from curses
-                            self.screen.command()
-                        except (curses.error, KeyError):
-                            # Ignore curses errors
-                            pass
-
+            # Keep track of game ticks
+            # speed basically determines the time a tick takes
+            # Try to get commands turing a tick
+                while not self.tick():
+                    self.screen.command()
                     # Limit CPU cycles, IMPORTANT
                     # When debugging or testing, DON'T sleep but go asap
                     if not self.debug:
-                        time.sleep(1)
+                        time.sleep(0.01)
+                    else:
+                        self.block.random_move()
 
-                # After every collision:
-                # Check if there is a full row in the grid
-                self.grid.row_is_full()
+                # After a tick passes, move the block down forcefully
+                self.block.down()
 
-        except KeyboardInterrupt as e:
-            if p_audio:
-                p_audio.terminate()
-            raise e
+            # After every collision:
+            # Check if there is a full row in the grid
+            self.grid.row_is_full()
 
+        self.screen.print("Game over!")
+        self.write_highscore()
+        time.sleep(3)
+
+        # There is not yet an endgame screen
         self.screen.endgame()
-        #refresh()
-        #stdscr.nodelay(False)
-        #report("Game Over!")
-        #write_highscore()
-
-        #if stdscr:
-        #    stdscr.getch()
-        try:
-            p_audio.terminate()
-        except:
-            pass
-
-    def check_score(self):
-        """
-        Check if the level and speed should be incremented
-        :return: Bool
-        """
-        if self.score // self.level >= 20:
-            self.level += 1
-            self.speed *= self.factor
-            self.screen.data()
-            return True
-        else:
-            return False
-
-def start_audio(self):
-    try:
-        from . import audio
-        p_audio = Process(target=audio.start)
-        p_audio.start()
-        return p_audio
-    except ImportError:
-        # simpleaudio is not installed
-        pass
-    pass
-
 
 class Queue(list):
     """
@@ -215,6 +179,7 @@ class Queue(list):
         random.shuffle(tmp)
         for b in tmp:
             self.append(b(game = self.game))
+
     def pop(self, i = 0):
         """
         Pop a block from the stack
@@ -223,201 +188,14 @@ class Queue(list):
         """
         if len(self) <= 2:
             self.fill()
+        block = super().pop(i)
+        # Draw the next block in the next box
         self.game.screen.next()
-        return super().pop(i)
+        return block
 
     def next(self):
         """
         Return the next block in the queue
         """
-        return self[-1]
-
-
-def start(init_stdscr=None, **kwargs):
-    """
-    Starts the main game loop
-    Initializes a block
-    Moves the block until there is a collision
-    Loop until game over
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--audio','-a',action='store_true', default=False)
-    args = parser.parse_args()
-    if args.audio:
-        global p_audio
-        try:
-            from . import audio
-            p_audio = Process(target=audio.start)
-            p_audio.start()
-        except ImportError:
-            # simpleaudio is not installed
-            pass
-
-
-    global grid
-    global gridsize
-    grid = Grid(gridsize)
-
-    global report
-    if init_stdscr:
-        global stdscr
-        stdscr = init_stdscr
-        refresh = refresh_curses
-        report = report_curses
-        border()
-        curses.curs_set(0)
-        stdscr.nodelay(True)
-        curses.use_default_colors()
-        for i in range(0, curses.COLORS):
-            curses.init_pair(i, -1, i);
-        
-    else:
-        refresh = refresh_debug
-        report = print
-        breakpoint()
-
-    global block_list
-    block_list = [
-            Block(block_type='random', insert_point=insert_point)
-            for _ in range(2)
-            ]
-
-    global score
-    report_score()
-    read_highscore()
-
-    global speed
-    global level
-
-    try:
-        while not grid.game_over:
-            grid.spawn(get_next_block())
-            show_next_block()
-            t0 = time.time()
-            while True:
-                if time.time() - t0 > speed:
-                    t0 = time.time()
-                    grid.block.move('down')
-                try:
-                    pick_move(get_move())
-                except (curses.error, KeyError):
-                    pass
-                if grid.collision():
-                    break
-                refresh()
-                time.sleep(0.01)
-
-            grid.put()
-            score += grid.full_row()
-            if score // level >= 20:
-                level += 1
-                speed *= factor
-            report_score()
-    except KeyboardInterrupt as e:
-        if p_audio:
-            p_audio.terminate()
-        raise e
-
-    refresh()
-    stdscr.nodelay(False)
-    report("Game Over!")
-    write_highscore()
-    if stdscr:
-        stdscr.getch()
-    try:
-        p_audio.terminate()
-    except:
-        pass
-
-def show_next_block():
-    for y in range(1,6):
-        y += 8
-        stdscr.addstr(y, 12+x_off, ' '*8)
-    global block_list
-    stdscr.addstr(8, 12+x_off, "Next:")
-    next_b = block_list[0]
-    y_ori = 9
-    x_ori = 10
-    for y, x in next_b:
-        m = next_b.mark
-        stdscr.addstr(y+y_ori, x+x_ori, ' ', curses.color_pair(m))
-
-
-def get_next_block():
-    global block_list
-    block_list.append(Block(block_type='random', insert_point=insert_point))
-    return block_list.pop(0)
-
-def pick_move(move):
-    if move == 'fw':
-        grid.block.rotate(move)
-    else:
-        grid.block.move(move)
-
-movelookup = {
-        'KEY_DOWN': 'down',
-        'KEY_LEFT': 'left',
-        'KEY_RIGHT': 'right',
-        'KEY_UP': 'fw'
-        }
-def get_move():
-    return movelookup[stdscr.getkey()]
-
-def clear_curses():
-    grid_y, grid_x = gridsize
-    for y in range(10):
-        stdscr.addstr(y+y_off, 0+x_off, " "*grid_x)
-
-def refresh_curses():
-    clear_curses()
-    for y, line in enumerate(grid[4:]):
-        for x, ch in enumerate(line):
-            if ch == 0:
-                ch = ' '
-                stdscr.addstr(y+y_off, x+x_off, ch)
-            else:
-                stdscr.addstr(y+y_off, x+x_off, " ", curses.color_pair(ch))
-                
-    refresh_block()
-    stdscr.refresh()
-
-def refresh_block():
-    for y, x in grid.block.position():
-        if y > 3:
-            Y = y+y_off-buff
-            X = x+x_off
-            ch = grid.block.mark
-            stdscr.addstr(Y, X, " ", curses.color_pair(grid.block.mark))
-
-def refresh_debug():
-    print(grid)
-
-def report_score():
-    global score
-    global level
-    stdscr.addstr(1, 12+x_off, "SCORE: "+str(score))
-    stdscr.addstr(2, 12+x_off, "LEVEL: "+str(level))
-    stdscr.refresh()
-
-def read_highscore():
-    global name
-    global highscore
-    try :
-        with open(score_file, 'r') as f: 
-            name = f.readline().strip()
-            highscore = f.readline().strip()
-    except:
-        pass
-    stdscr.addstr(4, 12+x_off, "HIGHSCORE:")
-    stdscr.addstr(5, 12+x_off, str(name))
-    stdscr.addstr(6, 12+x_off, str(highscore))
-
-def write_highscore():
-    if score > int(highscore):
-        import getpass
-        name = getpass.getuser()
-        with open(score_file, 'w') as f:
-            f.write(f"{name}\n")
-            f.write(f"{score}\n")
-
+        return self[0]
 
